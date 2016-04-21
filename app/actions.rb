b85e16807fc3096fa5dcfabebfc726db158bf2b6
Patrick Zhang <a00831168@gmail.com>
2016-04-21 18:35:53 +0000

    class 8

--- app/actions.rb
+++ app/actions.rb
@@ -1,13 +1,26 @@
-def humanized_time_ago(time_ago_in_minutes)
-  if time_ago_in_minutes >= 60
-    "#{time_ago_in_minutes / 60} hours ago"
-  else
-    "#{time_ago_in_minutes} minutes ago"
-  end
+get '/' do
+  @posts = Post.order(created_at: :desc)
+  erb(:index)
 end
 
-get '/' do
+get '/signup' do
+  @user = User.new
+  erb :signup
+end
 
-  @posts = Post.all
-  erb(:index)
+# ...
+
+post '/signup' do
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+  if @user.save
+    "User #{username} saved!"
+  else
+    erb(:signup)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-
+  has_many :comments
+  has_many :likes
+  has_many :posts
+  
+  validates_presence_of :email, :avatar_url, :username, :password
+  validates_uniqueness_of :email, :username
+  
 end
