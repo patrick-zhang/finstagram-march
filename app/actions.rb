dbaf1e16c2f17e553196168431c4dc6cf1838ee4
Patrick Zhang <a00831168@gmail.com>
2016-04-25 22:56:20 +0000

    class 9

--- app/actions.rb
+++ app/actions.rb
@@ -3,12 +3,12 @@ get '/' do
   erb(:index)
 end
 
+
 get '/signup' do
   @user = User.new
   erb :signup
 end
 
-# ...
 
 post '/signup' do
   email      = params[:email]
@@ -19,8 +19,40 @@ post '/signup' do
   @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
 
   if @user.save
-    "User #{username} saved!"
+    redirect(to('/login'))
   else
     erb(:signup)
+  end
+end
+
+
+get '/login' do    # when a GET request comes into /login
+  erb(:login)      # render app/views/login.erb
+end
+
+
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)  
+
+  if user && user.password == password
+    session[:user_id] = user.id
+    redirect(to('/'))
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect(to('/'))
+end
+
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
   end
 end
