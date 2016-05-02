cf53c4cb96786d7bfd167ed152cd375b2465af77
Patrick Zhang <a00831168@gmail.com>
2016-05-02 20:18:47 +0000

    class day 11

--- app/actions.rb
+++ app/actions.rb
@@ -78,8 +78,39 @@ post '/posts' do
   end
 end
 
+post '/comments' do
+  # point values from params to variables
+  text = params[:text]
+  post_id = params[:post_id]
+
+  # instantiate a comment with those values & assign the comment to the `current_user`
+  comment = Comment.new({ text: text, post_id: post_id, user_id: current_user.id })
+
+  # save the comment
+  comment.save
+
+  # `redirect` back to wherever we came from
+  redirect(back)
+end
+
+post '/likes' do
+  post_id = params[:post_id]
+
+  like = Like.new({ post_id: post_id, user_id: current_user.id })
+  like.save
+
+  redirect(back)
+end
+
+delete '/likes/:id' do
+  like = Like.find(params[:id])
+  like.destroy
+  redirect(back)
+end
+
 helpers do
   def current_user
     User.find_by(id: session[:user_id])
   end
 end
+
